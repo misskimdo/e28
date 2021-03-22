7f7ae350524d01a29fa05d3efc968275f6a65765
Kim Do <miss.kim.do@gmail.com>
2021-03-21 22:29:53 -0500

    project 1 files

--- p1/KD_game_vue.js
+++ p1/KD_game_vue.js
@@ -1,16 +1,85 @@
-  const Game = {
-      data() {
-          return {
-              options: ['rock', 'paper', 'scissors'],
-              matrixOption: ''
-          }
-      },
-      methods: {
-          play: function () {
-              var matrixPlay = Math.floor(Math.random() * this.options.length);
-              this.matrixOption = this.options[matrixPlay];
-          }
-      }
-  }
+const Game = {
+    data() {
+        return {
+            options: ['rock', 'paper', 'scissors'],
+            matrixOption: '',
+            playerOption: '',
+            youWon: null,
+            youLost: null,
+            tie: null,
+            userScore: 0,
+            matrixScore: 0,
+            feedback: '',
+            winners: [],
+        }
+    },
+    methods: {
 
-  const app = Vue.createApp(Game).mount('#app');
+        play(option) {
+            this.playerOption = option;
+
+            const matrixPlay = Math.floor(Math.random() * this.options.length);
+            this.matrixOption = this.options[matrixPlay];
+
+            this.score();
+
+        },
+
+        score() {
+            let playerOption = this.playerOption;
+            let matrixOption = this.matrixOption;
+            let winners = this.winners;
+
+            if (playerOption == matrixOption) {
+                this.feedback = "It's a tie! 👔";
+                this.youWon = false;
+                this.youLost = false;
+                this.tie = true;
+                this.winners.push('It was a tie.');
+            } else if (playerOption == "rock" && matrixOption == "scissors") {
+                this.feedback = "You win! 😄";
+                this.userScore++;
+                this.youWon = true;
+                this.winners.push('You won.');
+            } else if (playerOption === "paper" && matrixOption === "rock") {
+                this.feedback = "You win! 😄";
+                this.userScore++;
+                this.youWon = true;
+                this.winners.push('You won.');
+            } else if (playerOption === "scissors" && matrixOption === "paper") {
+                this.feedback = "You win! 😄";
+                this.userScore++;
+                this.youWon = true;
+                this.winners.push('You won.');
+            } else {
+                this.feedback = "The Matrix wins. 😔";
+                this.matrixScore++;
+                this.youLost = true;
+                this.youWon = false;
+                this.winners.push('The Matrix won.');
+            }
+        },
+
+        resetGame() {
+            this.feedback = '';
+            this.userScore = 0;
+            this.matrixScore = 0;
+            this.matrixOption = '';
+            this.playerOption = '';
+            this.winners = [];
+        }
+    }
+}
+
+const GameHistory = {
+    name: 'GameHistory',
+    props: ['winner', 'index'],
+    data() {
+        return {}
+    },
+    template: '#game-history'
+}
+
+const app = Vue.createApp(Game)
+app.component('game-history', GameHistory);
+app.mount('#app');
--- p1/README.md
+++ p1/README.md
@@ -7,4 +7,16 @@
 
 [vue js 3 random](https://www.minddevelopmentanddesign.com/blog/random-array-picker-with-vue-for-beginners/)
 
+[add items to an array in components](https://stackoverflow.com/questions/50650815/vue-add-a-component-on-button-click/50651082)
+
+[icon vectors](https://www.freepik.com/premium-vector/hand-gesture-icon-set_6539624.htm#page=1&query=rock%20paper%20scissors&position=17)
+
+[Nunito Font](https://fonts.google.com/specimen/Nunito?preview.text_type=custom#standard-styles)
+
+[Big Shoulders Display Font](https://fonts.google.com/specimen/Big+Shoulders+Display?preview.text_type=custom)
+
+[item](link)
+
+[item](link)
+
 ## Notes for Instructor
