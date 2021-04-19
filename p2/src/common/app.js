c1bad71abd49d8565c4a0f6c44ec62dcbc06a83a
Kim Do <miss.kim.do@gmail.com>
2021-04-18 22:14:50 -0500

    update  axios configs to .me

--- p2/src/common/app.js
+++ p2/src/common/app.js
@@ -1,4 +1,4 @@
 export const axios = require('axios').create({
-    baseURL: process.env.VUE_APP_API_URL ?? 'http://e28api.kimdo.loc',
+    baseURL: process.env.VUE_APP_API_URL ?? 'http://e28api.kimdo.me',
     responseType: 'json'
 })
